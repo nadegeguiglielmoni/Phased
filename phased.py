8e301c7549a8fb97b6f846ca854d8eea9f8b0f67
Nadège Guiglielmoni <nguiglielmoni@wanadoo.fr>
2024-02-15 18:28:25 +0100

    Create phased.py

---
+++ phased.py
@@ -0,0 +1,55 @@
+infile = open("dups.bed", "r")
+
+haplotigs = {}
+
+for line in infile.readlines():
+	line = line.strip().split()
+	if "HAPLOTIG" in line[3]:
+		if line[0] not in haplotigs.keys():
+			haplotigs[line[0]] = {}
+		haplotigs[line[0]][line[4]] = []
+
+#print(haplotigs)
+
+infile = open("minimap2_hifi.Ppr.italy.phased.fasta.paf", "r")
+
+count_reads = {}
+reads_coord = {}
+
+for line in infile.readlines():
+	line = line.strip().split()
+	if line[0] not in reads_coord.keys():
+		coord = (str(line[5]), int(line[2]), int(line[3]))
+		reads_coord[str(line[0])] = [coord]
+	else :
+		coord = (str(line[5]), int(line[2]), int(line[3]))
+		reads_coord[str(line[0])].append(coord)
+
+#print(reads_coord)
+
+for read in reads_coord.keys():
+	if read not in count_reads.keys():
+		count_reads[read] = 0
+	for i in range(0, len(reads_coord[read])):
+		for j in range(1, len(reads_coord[read])):
+			#print(read, reads_coord[read])
+			if reads_coord[read][i][0] is not reads_coord[read][j][0]:
+				if reads_coord[read][i][1] < reads_coord[read][j][1] and reads_coord[read][i][2] < reads_coord[read][j][2] and reads_coord[read][i][2] <= reads_coord[read][j][1]:
+					#print(reads_coord[read][i][0],reads_coord[read][j][0])
+					if reads_coord[read][i][0] in haplotigs.keys() and reads_coord[read][j][0] in haplotigs[reads_coord[read][i][0]].keys():
+						if read not in haplotigs[reads_coord[read][i][0]][reads_coord[read][j][0]]:
+							haplotigs[reads_coord[read][i][0]][reads_coord[read][j][0]].append(read)
+					elif reads_coord[read][j][0] in haplotigs.keys() and j in haplotigs[reads_coord[read][j][0]].keys():
+						if read not in haplotigs[reads_coord[read][i][0]][reads_coord[read][j][0]]:
+							haplotigs[reads_coord[read][j][0]][reads_coord[read][i][0]].append(read)
+
+#print(haplotigs)
+
+count = 0
+
+for i in haplotigs.keys():
+	for j in haplotigs[i].keys(): 
+		print(i, j, len(haplotigs[i][j]))
+		count = count + len(haplotigs[i][j])
+
+print(count)
